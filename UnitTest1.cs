3e16c7d9a8ed2d07e486506b87ca5f5268d955b0
aniruddhabasagare28 <aniruddhabasagare28@gmail.com>
2018-02-19 12:18:33 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+        }
+    }
+}
